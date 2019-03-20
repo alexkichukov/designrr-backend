6bbfa0dcd260af01529523f07b922a95ba7ce54b
Alex K <contact_allex@yahoo.com>
2019-03-20 16:49:41 +0200

    Simplify the api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const _ = require('lodash')
-const fs = require('fs')
+const branding = require('./briefs/branding.json')
+const ui = require('./briefs/ui.json')
 const PORT = process.env.PORT || 5000
 const app = express()
 
@@ -25,60 +26,34 @@ app.get('/any', (req, res) => {
   let category = _.random(1)
   // 0 is branding, 1 is ui
   if (category === 0) {
-    fs.readFile('./briefs/branding.json', (err, data) => {
-      // If there is an error
-      if (err) res.json({ error: true })
-  
-      // Get a random branding brief
-      let branding = JSON.parse(data)
-      let index = _.random(branding.length - 1)
-      res.json({
-        brief: branding[index]
-      })
+    let index = _.random(branding.length - 1)
+    res.json({
+      brief: branding[index]
     })
   }
   else if (category === 1) {
-    fs.readFile('./briefs/ui.json', (err, data) => {
-      // If there is an error
-      if (err) res.json({ error: true })
-  
-      // Get a random branding brief
-      let ui = JSON.parse(data)
-      let index = _.random(ui.length - 1)
-      res.json({
-        brief: ui[index]
-      })
+    let index = _.random(ui.length - 1)
+    res.json({
+      brief: ui[index]
     })
   }
 })
 
 // Branding
 app.get('/branding', (req, res) => {
-  fs.readFile('./briefs/branding.json', (err, data) => {
-    // If there is an error
-    if (err) res.json({ error: true })
-
-    // Get a random branding brief
-    let branding = JSON.parse(data)
-    let index = _.random(branding.length - 1)
-    res.json({
-      brief: branding[index]
-    })
+  // Get a random branding brief
+  let index = _.random(branding.length - 1)
+  res.json({
+    brief: branding[index]
   })
 })
 
 // UI
 app.get('/ui', (req, res) => {
-  fs.readFile('./briefs/ui.json', (err, data) => {
-    // If there is an error
-    if (err) res.json({ error: true })
-
-    // Get a random branding brief
-    let ui = JSON.parse(data)
-    let index = _.random(ui.length - 1)
-    res.json({
-      brief: ui[index]
-    })
+  // Get a random branding brief
+  let index = _.random(ui.length - 1)
+  res.json({
+    brief: ui[index]
   })
 })
 
